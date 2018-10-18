2d9b347f25ae49394d61fe95b9b3b5d4aca9a36a
hirdaytripathi <hriday2010@live.in>
2018-10-18 19:38:31 +0530

    Initial commit

---
+++ src/main/java/kp/example/demo2/MainController.java
@@ -0,0 +1,14 @@
+package kp.example.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "home.html";
+  }
+}
