c37547b737ce919c6cbb22a6f423aa365bf7e022
hirdaytripathi <hriday2010@live.in>
2018-10-18 20:00:24 +0530

    this is my first commit

--- src/main/java/kp/example/demo2/MainController.java
+++ src/main/java/kp/example/demo2/MainController.java
@@ -1,5 +1,8 @@
 package kp.example.demo2;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +12,9 @@ public class MainController {
 
   @RequestMapping("/")
   public String index() {
+	  System.out.println("hi there");
+	  List<Integer> lst = Arrays.asList(1,2,3,4,5,6,7);
+	  lst.forEach(System.out::println);
     return "home.html";
   }
 }
